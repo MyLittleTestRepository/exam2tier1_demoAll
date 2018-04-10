b6262a354c44d0fc2bc23c8e6fb8fe421b0ec3b5
Averin Dmitry <averin@intaro.email>
2018-04-10 11:59:58 +0300

    ex2-88 end. perfomance statistic

--- ex2/.left.menu.php
+++ ex2/.left.menu.php
@@ -1,6 +1,13 @@
 <?
 $aMenuLinks = Array(
 	Array(
+		"Оценка производительности", 
+		"/ex2/time_control/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
 		"Форма обратной связи", 
 		"/ex2/feedback/", 
 		Array(), 
@@ -8,74 +15,74 @@ $aMenuLinks = Array(
 		"" 
 	),
 	Array(
-		"Простой компонент_49",
-		"/ex2/simplecomp_49/",
-		Array(),
-		Array(),
-		""
-	),
-	Array(
-		"Простой компонент_58",
-		"/ex2/simplecomp_58/",
-		Array(),
-		Array(),
-		""
-	),
-	Array(
-		"Простой компонент_60",
-		"/ex2/simplecomp_60/",
-		Array(),
-		Array(),
-		""
-	),
-	Array(
-		"Простой компонент_70",
-		"/ex2/simplecomp_70/",
+		"Простой компонент_49", 
+		"/ex2/simplecomp_49/", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
-		"Простой компонент_71",
-		"/ex2/simplecomp_71/",
-		Array(),
-		Array(),
-		""
+		"Простой компонент_58", 
+		"/ex2/simplecomp_58/", 
+		Array(), 
+		Array(), 
+		"" 
 	),
 	Array(
-		"Простой компонент_81",
-		"/ex2/simplecomp_81/",
-		Array(),
-		Array(),
-		""
+		"Простой компонент_60", 
+		"/ex2/simplecomp_60/", 
+		Array(), 
+		Array(), 
+		"" 
 	),
 	Array(
-		"Простой компонент_82",
-		"/ex2/simplecomp_82/",
-		Array(),
-		Array(),
-		""
+		"Простой компонент_70", 
+		"/ex2/simplecomp_70/", 
+		Array(), 
+		Array(), 
+		"" 
 	),
 	Array(
-		"Простой компонент_97",
-		"/ex2/simplecomp_97/",
-		Array(),
-		Array(),
-		""
+		"Простой компонент_71", 
+		"/ex2/simplecomp_71/", 
+		Array(), 
+		Array(), 
+		"" 
 	),
 	Array(
-		"Простой компонент_100",
-		"/ex2/simplecomp_100/",
-		Array(),
-		Array(),
-		""
+		"Простой компонент_81", 
+		"/ex2/simplecomp_81/", 
+		Array(), 
+		Array(), 
+		"" 
 	),
 	Array(
-		"Простой компонент_107",
-		"/ex2/simplecomp_107/",
-		Array(),
-		Array(),
-		""
+		"Простой компонент_82", 
+		"/ex2/simplecomp_82/", 
+		Array(), 
+		Array(), 
+		"" 
 	),
+	Array(
+		"Простой компонент_97", 
+		"/ex2/simplecomp_97/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Простой компонент_100", 
+		"/ex2/simplecomp_100/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Простой компонент_107", 
+		"/ex2/simplecomp_107/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
 );
 ?>
---
+++ ex2/time_control/index.php
@@ -0,0 +1,8 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности");
+?>ex2-88:
+    </br>    /products/index.php     24.85%
+    </br>   -11kb
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
