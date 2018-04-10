390f7d064c5cc6b3faf1e7bb4fb6b2bc3788e208
Averin Dmitry <averin@intaro.email>
2018-04-10 12:45:45 +0300

    ex2-10 end. perfomance statistic

--- ex2/time_control/index.php
+++ ex2/time_control/index.php
@@ -4,5 +4,9 @@ $APPLICATION->SetTitle("Оценка производительности");
 ?>ex2-88:
     </br>    /products/index.php     24.85%
     </br>   -11kb
+    </br></br>
+    ex2-10:
+    </br>    /products/index.php     20.73%
+    </br>   bitrix:catalog: 0.1594 с
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
