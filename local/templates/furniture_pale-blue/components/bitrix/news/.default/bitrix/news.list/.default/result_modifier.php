df10310bbe25adb58e222b8c2f54026ac3594c94
Averin Dmitry <averin@intaro.email>
2018-04-11 11:53:21 +0300

    ex2-34 fix. specialdate. add check date

--- local/templates/furniture_pale-blue/components/bitrix/news/.default/bitrix/news.list/.default/result_modifier.php
+++ local/templates/furniture_pale-blue/components/bitrix/news/.default/bitrix/news.list/.default/result_modifier.php
@@ -1,7 +1,7 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
 	die();
 
-if ($arParams["SET_SPECIALDATE"] == "Y")
+if ($arParams["SET_SPECIALDATE"] == "Y" and $arResult['ITEMS'][0]['ACTIVE_FROM'])
 {
 	$arResult['SPECIALDATE'] = $arResult['ITEMS'][0]['ACTIVE_FROM'];
 	$this->__component->setResultCacheKeys(['SPECIALDATE']);
